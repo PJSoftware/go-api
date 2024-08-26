da98e01e34148a47f0ed18e2ab43656d699600ae
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-08-26 14:07:40 +1000

    feat: add endpoint caching to return singleton per URL

--- endpoint.go
+++ endpoint.go
@@ -2,6 +2,12 @@ package api
 
 import "strings"
 
+// Because we probably want to apply rate limiting to our API at the endpoint
+// level, we need to cache our endpoints. Code which calls NewEndpoint()
+// multiple times needs to return the same Endpoint object every time, so that
+// its limiting (etc) can apply correctly across the lifetime of the program.
+var epCache = make(map[string]*Endpoint)
+
 // Each Endpoint should be individually managed by the client code. An Endpoint
 // is generated via api.NewEndpoint()
 type Endpoint struct {
@@ -9,15 +15,30 @@ type Endpoint struct {
 	parent      *APIData
 }
 
+// epCacheKey() generates a key for our endpoint cache.
+//
+// the returned key currently matches ep.URL() -- but that is not a functional
+// requirement, and is likely to change if appropriate.
+func (a *APIData) epCacheKey(epURL string) string {
+	return a.rootURL + "/" + epURL
+}
+
 // (*APIData).NewEndpoint returns a new *Endpoint object
 func (a *APIData) NewEndpoint(epURL string) *Endpoint {
-	ep := &Endpoint{}
 	epURL = strings.TrimPrefix(epURL, a.rootURL)
 	epURL = strings.TrimPrefix(epURL, "/")
+
+	epCacheKey := a.epCacheKey(epURL)
+	if ep, ok := epCache[epCacheKey]; ok {
+		return ep
+	}
+
+	ep := Endpoint{}
 	ep.endpointURL = epURL
 	ep.parent = a
 
-	return ep
+	epCache[epCacheKey] = &ep
+	return &ep
 }
 
 // (*Endpoint).URL() returns the full URL for that endpoint
--- endpoint_test.go
+++ endpoint_test.go
@@ -56,3 +56,38 @@ func TestNewEndpoint(t *testing.T) {
 		}
 	})
 }
+
+func TestEndpointIsSingleton(t *testing.T) {
+	root := "http://test.io"
+	epURL1 := "endpoint1"
+	epURL2 := "endpoint2"
+
+	t.Run("NewEndpoint returns singleton", func(t *testing.T) {
+		api := goapi.New(root)
+		ep1 := api.NewEndpoint(epURL1)
+		ep2 := api.NewEndpoint(epURL1)
+
+		if ep1 != ep2 {
+			t.Errorf("two endpoints should match, but do not: %v / %v", ep1, ep2)
+		}
+	})
+
+	t.Run("Endpoint caching pre-processes URL", func(t *testing.T) {
+		api := goapi.New(root)
+		ep1 := api.NewEndpoint(epURL1)
+		ep2 := api.NewEndpoint(root + "/" + epURL1)
+
+		if ep1 != ep2 {
+			t.Errorf("endpoint caching fails to detect equivalence: %v / %v", ep1, ep2)
+		}
+	})
+
+	t.Run("Different endpoint per url", func(t *testing.T) {
+		api := goapi.New(root)
+		ep1 := api.NewEndpoint(epURL1)
+		ep2 := api.NewEndpoint(epURL2)
+
+		if ep1 == ep2 {
+			t.Errorf("does not return unique EP: %v / %v", ep1, ep2)
+		}
+	})}
