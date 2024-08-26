695f8a51a373ed7838bf6026fe551100f1a6f6ef
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-08-26 13:05:58 +1000

    task: bump version; add sample rate-limiting code

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package api
 import "fmt"
 
 const pkgName = "go-api"
-const pkgVersion = "v0.4.15"
+const pkgVersion = "v0.4.16"
 
 // api.Version() returns the current package version
 func (a *APIData) Version() string {
