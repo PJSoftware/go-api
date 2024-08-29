72701df42eb2f7da9e22206f1670cbe935b40b35
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-08-29 14:38:16 +1000

    task: bump version

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package api
 import "fmt"
 
 const pkgName = "go-api"
-const pkgVersion = "v0.4.18"
+const pkgVersion = "v0.4.19"
 
 // api.Version() returns the current package version
 func (a *APIData) Version() string {
